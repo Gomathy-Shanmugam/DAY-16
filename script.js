3ad22c4c29e52d2a3af5d3cf7454e135884cfece
Gomathy <113160365+Gomathy-Shanmugam@users.noreply.github.com>
2023-12-20 13:12:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+// function time(){
+//     document.getElementById("container").innertext += "one";
+// }
+
+var timeout= setTimeout(() => {
+    document.getElementById("container").innerText += 10
+    setTimeout(() => {
+        container.innerText = "";
+    setTimeout(() => {
+            document.getElementById("container").innerText += 9
+           setTimeout(() => {
+                container.innerText = "";
+    setTimeout(() => {
+                    document.getElementById("container").innerText += 8
+            setTimeout(() => {
+                        container.innerText = "";
+    setTimeout(() => {
+                    document.getElementById("container").innerText += 7
+            setTimeout(() => {
+                    container.innerText = "";
+    setTimeout(() => {
+                    document.getElementById("container").innerText += 6
+            setTimeout(() => {
+                    container.innerText = "";
+                    setTimeout(() => {
+                        document.getElementById("container").innerText += 5
+                        setTimeout(() => {
+                            container.innerText = "";
+                            setTimeout(() => {
+                                document.getElementById("container").innerText += 4
+                                setTimeout(() => {
+                                    container.innerText = "";
+                                    setTimeout(() => {
+                                        document.getElementById("container").innerText += 3
+                                        setTimeout(() => {
+                                            container.innerText = "";
+                                            setTimeout(() => {
+                                                document.getElementById("container").innerText += 2
+                                                setTimeout(() => {
+                                                    container.innerText = "";
+                                                    setTimeout(() => {
+                                                        document.getElementById("container").innerText += 1
+                                                        setTimeout(() => {
+                                                            container.innerText = "";
+                                                            setTimeout(() => {
+                                                                document.getElementById("container").innerText += "HAPPY INDEPENDENCE DAY"
+                                                                setTimeout(() => {
+                                                                    container.innerText = "";
+                                                        
+                    
+
+    }, 1000);
+},1000); 
+}, 1000);
+}, 1500);
+
+    }, 1000);
+}, 1000);
+
+    }, 1000);
+}, 1000);
+
+
+}, 1000);
+    },1000);  
+
+}, 1000);
+}, 1000);
+
+}, 1000);
+}, 1000);
+
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);  
+}, 1000);
+}, 1000);
+    
+},1000);
+},1000);
+
+
+
+
+
+
+
+
+
+    
+
